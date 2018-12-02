edb13e90413cdcaa2412460ea7953340a04e246b
Erik Kundt <bitshift@posteo.org>
2018-12-02 01:16:43 +0100

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+// extern crate bnf;
+// use bnf::Grammar;
+
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "yul.pest"]
+struct YulParser;
+
+fn main() {
+    let input = "{
+        function f() {
+
+        }
+        let _1 := mload(0)
+        let f_a := mload(1)
+        let f_r
+        {
+            f_a := mload(f_a)
+            f_r := add(f_a, calldatasize())
+        }
+        let z := mload(2)
+
+        if mload(2) {
+            f_a := mload(f_a)
+        }
+
+        let a := 1
+        switch calldataload(0)
+        case 0 {
+            mstore(0, 1)
+            a := 8
+        }
+        default {
+            a := 3
+            a := 4
+        }
+        a := 5
+    }";
+    let result = YulParser::parse(Rule::Block, input).unwrap_or_else(|e| panic!("{}", e));
+    println!("{:?}", result);
+}
